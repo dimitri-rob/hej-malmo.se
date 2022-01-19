6741295e89fce27444e668384214436418cf5ab5
Dimitri <contact@dimic.eu>
2022-01-19 23:13:53 +0100

    Create Release.vue

---
+++ src/views/Release.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h1>{{ data[release].artist }}</h1>
+		<h2>{{ data[release].name }}</h2>
+
+		<ul>
+			<li v-for="link in data[release].links" :key="link.item">
+				<a :href="link.href" target="_blank">{{ link.item }}</a>
+			</li>
+		</ul>
+
+		<img :src="`${publicPath}assets/artworks/${release}.jpg`" />
+	</div>
+</template>
+
+<script>
+import releaseData from "@/assets/releases.json";
+
+export default {
+	data() {
+		return {
+			publicPath: process.env.BASE_URL,
+			release: this.$route.params.id,
+			data: releaseData,
+		};
+	},
+};
+</script>
