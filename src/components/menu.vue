e64f0a532fdf5e1fff2d211363ed6830937e70ea
Dimitri <contact@dimic.eu>
2022-01-19 21:25:31 +0100

    Menu items loop

---
+++ src/components/menu.vue
@@ -0,0 +1,52 @@
+<template>
+	<nav class="header__menu">
+		<ul>
+			<li v-for="item in items" :key="item.name">
+				<a :href="item.href" target="_blank">{{ item.name }}</a>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			items: [
+				{
+					name: "Spotify",
+					href: "https://open.spotify.com/artist/6yBdtaTSKKjyUZI2xH052v",
+				},
+				{
+					name: "Bandcamp",
+					href: "https://hej-malmo.bandcamp.com/",
+				},
+				{
+					name: "Youtube",
+					href: "https://www.youtube.com/channel/UCFkOfes-e0HMlL_dcKv4xVQ",
+				},
+				{
+					name: "Soundcloud",
+					href: "https://soundcloud.com/hej-malmo",
+				},
+				{
+					name: "Instagram",
+					href: "https://www.instagram.com/hej_malmo/",
+				},
+				{
+					name: "Facebook",
+					href: "https://www.facebook.com/hejmalmo",
+				},
+				{
+					name: "Twitter",
+					href: "https://twitter.com/hej_malmo",
+				},
+				{
+					name: "Merch",
+					href: "https://hej-malmo.bandcamp.com/merch",
+				},
+			],
+		};
+	},
+};
+</script>
