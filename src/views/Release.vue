842a60e2fa8322d8d367357ac8f27aa201d204f1
Dimitri <contact@dimic.eu>
2022-01-21 00:15:48 +0100

    Tailwind

--- src/views/Release.vue
+++ src/views/Release.vue
@@ -1,17 +1,18 @@
 <template>
 	<div>
+		<img :src="`${publicPath}assets/artworks/${release}.jpg`" />
 		<h1>{{ data[release].artist }}</h1>
 		<h2>{{ data[release].name }}</h2>
 
 		<ul>
 			<li v-for="link in data[release].links" :key="link.item">
-				<a :href="link.href" target="_blank">{{ link.item }}</a>
-				<img :src="`${publicPath}assets/logos/${link.item}.svg`" />
-				{{ link.text }}
+				<a :href="link.href" target="_blank">
+					<img :src="`${publicPath}assets/logos/${link.item}.svg`" />
+				</a>
+
+				<a :href="link.href" target="_blank">{{ link.text }}</a>
 			</li>
 		</ul>
-
-		<img :src="`${publicPath}assets/artworks/${release}.jpg`" />
 	</div>
 </template>
 
