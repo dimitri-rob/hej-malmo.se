a85ad54ddb5923b6cbfd7465ed724c6e09e176c8
Dimitri <contact@dimic.eu>
2022-01-25 00:36:38 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
-import VuePageTransition from 'vue-page-transition'
 
-createApp(App).use(router, VuePageTransition).mount('#app')
+
+createApp(App).use(router).mount('#app')
